ca1964da70ade0f05235d5b334dc6a2dbd544a7e
Lucas Ferreira <49406941+luckeiros@users.noreply.github.com>
2024-03-17 18:25:41 -0300

    setting image ext

--- app/src/main/java/com/luckeiros/ticketandroid/common/extension/ImageExt.kt
+++ app/src/main/java/com/luckeiros/ticketandroid/common/extension/ImageExt.kt
@@ -3,13 +3,12 @@ package com.luckeiros.ticketandroid.common.extension
 import android.content.Context
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.GlideException
 import com.luckeiros.ticketandroid.R
 import com.luckeiros.ticketandroid.common.view.TopRoundedCornersTransformation
 
-fun loadImage(context: Context, url: String, image: ImageView) {
+fun ImageView.loadImage(context: Context, url: String) {
     Glide.with(context)
         .load(url)
         .transform(TopRoundedCornersTransformation(context.resources.getDimension(R.dimen.small_grid)))
-        .into(image)
+        .into(this)
 }
